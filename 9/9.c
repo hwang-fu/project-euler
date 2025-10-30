7c9ceaeb699447cb5ff03c75ca5ce4fa8a8263b0
hwang-fu <junzhe.hwangfu@gmail.com>
2025-10-30 17:29:27 +0100

    template

---
+++ 9/9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/// Idea:
+/// a + b + c = 1000
+/// a^2 + b^2 = c^2
+/// a < b < c
+/// => c = sqrt(a^2 + b^2)
+/// => a + b + sqrt(a^2 + b^2) = 1000
+/// => ...
+
+void solve()
+{
+
+    fprintf(stdout, "triplet not found.\n");
+}
+
+int main()
+{
+    solve();
+    return 0;
+}

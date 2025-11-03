6f19ae03d6802bcd0220b86eaad23c6b6ebec729
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-03 15:30:53 +0100

    solve(20): TypeScript

--- 20/20-ts.ts
+++ 20/20-ts.ts
@@ -1,3 +1,30 @@
+const K: bigint = 100n;
 
+const Memo: bigint[] = new Array(Number(K) + 1).fill(0n);
 
-const K: BigInt = 100n;
+function Factorial(n: bigint): bigint {
+    if (n === 0n) return 1n;
+
+    if (Memo[Number(n)] != 0n)
+    {
+        return Memo[Number(n)];
+    }
+
+    Memo[Number(n)] = n * Factorial(n - 1n);
+    return Memo[Number(n)];
+};
+
+function main() {
+    let result = Factorial(K);
+    let sum = 0n;
+
+    while (result)
+    {
+        sum += (result % 10n);
+        result /= 10n;
+    }
+
+    console.log(sum);
+}
+
+main();

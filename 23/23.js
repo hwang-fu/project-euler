3fc8b1747299fa72f29c7919d60097ec808e3368
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-04 18:45:31 +0100

    solve(23): JavaScript

---
+++ 23/23.js
@@ -0,0 +1,35 @@
+
+const UPPER_LIMIT = 28123;
+
+const AbundantNumbers = [];
+
+const isAbundantNumber = (n) => {
+    if (n < 12) return false;
+    let sum = 0;
+    for (let i = 1; i <= n / 2; i++) {
+        if (n % i == 0) sum += i;
+    }
+    return sum > n;
+}
+
+const isSumOfTwoAbundantNumbers = (n) => {
+    for (const abundantNumber of AbundantNumbers) {
+        if (n <= abundantNumber) continue;
+        const diff = n - abundantNumber;
+        if (AbundantNumbers.includes(diff)) return true;
+    }
+    return false;
+}
+
+function main() {
+    let sum = 0;
+    for (let i = 1; i <= UPPER_LIMIT; i++) {
+        if (isAbundantNumber(i)) AbundantNumbers.push(i);
+    }
+    for (let i = 1; i <= UPPER_LIMIT; i++) {
+        if (!isSumOfTwoAbundantNumbers(i)) sum += i;
+    }
+    console.log(sum);
+}
+
+main();

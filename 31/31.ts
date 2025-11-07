ecfb126169ce95c476bf0344a792aeae79754767
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-07 21:38:16 +0100

    solve(31): TypeScript

---
+++ 31/31.ts
@@ -0,0 +1,28 @@
+const K = 200;
+
+const Coins = [200, 100, 50, 20, 10, 5, 2, 1];
+
+const CoinSums = (amount: number, startIdx: number) => {
+    if (amount < 0) {
+        return 0;
+    }
+
+    if (amount === 0) {
+        return 1;
+    }
+
+    let sum = 0;
+
+    for (let i = startIdx; i < Coins.length; i++) {
+        sum += CoinSums(amount - Coins[i], i);
+    }
+
+    return sum;
+};
+
+(() => {
+    const combinations = CoinSums(K, 0);
+    console.log(combinations);
+})();
+
+export {};
